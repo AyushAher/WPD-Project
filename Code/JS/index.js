4b2d5d97f1dc3bd8e8858e8dbb1a696322450999
AyushAher <ayushaher118@gmail.com>
2022-05-22 16:07:44 +0530

    cart fully functional

--- Code/JS/index.js
+++ Code/JS/index.js
@@ -1,3 +1,6 @@
+SetCartItems()
+
+
 function AddToCart(prodId) {
     var cart = JSON.parse(localStorage.getItem('cart')) // get cart data
     var price = document.getElementById(prodId).innerHTML.substring(1);// to remove the $ from the span tag value
@@ -6,20 +9,67 @@ function AddToCart(prodId) {
     if (cart) {  //if cart is not empty or null
         var product = cart.find(x => x.prodId == prodId) // find the particular product in the cart array
         if (!product) { // if the product is not in the array then....
-            let qty = 1;
-            product = { qty, price, prodId }
+            var qty = document.getElementsByName(`qty_${prodId}`)[0].value; // Get The Qty available on the screen
+            qty = parseInt(qty);
+            if (qty == 0) qty++;
+            product = { qty, price: price * qty, prodId, mrp: price }
             cart.push(product) // add product to the cart array
         }
         else if (product) { // id the product is in the array then....
-            product.qty++; // increment the qty by 1
+            var qty = document.getElementsByName(`qty_${prodId}`)[0].value; // Get The Qty available on the screen
+            qty = parseInt(qty);
+            if (qty == product.qty) qty++
+            product.qty = qty;
             product.price = product.qty * price; // set the new price by multiplying the qty and the product price 
         }
     }
     else {
         cart = [] // set cart to an empty array
-        var product = { prodId, qty: 1, price } // declaring the product object
+        var qty = document.getElementsByName(`qty_${prodId}`)[0].value; // Get The Qty available on the screen
+        qty = parseInt(qty);
+        if (qty == 0) qty++;
+        var product = { prodId, qty, price: price * qty, mrp: price } // declaring the product object
         cart.push(product) // adding product to the cart
     }
 
     localStorage.setItem('cart', JSON.stringify(cart)) // storing the array in localstorage
+    SetCartItems()
+}
+
+function Plus(name) {
+    var input = document.getElementsByName(name)[0]
+    var inp = input.value
+    input.value = 0;
+    if (inp) {
+        inp = parseInt(inp);
+        inp++;
+        input.value = inp;
+    }
+}
+
+function Minus(name) {
+    var input = document.getElementsByName(name)[0]
+    var inp = input.value
+    if (inp) {
+        inp = parseInt(inp);
+        inp--;
+        input.value = inp;
+        var cart = JSON.parse(localStorage.getItem('cart'))
+        var product = cart.find(x => x.prodId == name.substring(4))
+        product.qty = inp;
+        product.price -= product.mrp
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }
+}
+
+// qty should not be negative
+if (input.value < 0) {
+    input.value = 0;
+}
+
+function SetCartItems() {
+    var cart = JSON.parse(localStorage.getItem('cart'))
+    cart.forEach(x =>
+        document.getElementsByName(`qty_${x.prodId}`)[0].value = x.qty
+    );
 }
